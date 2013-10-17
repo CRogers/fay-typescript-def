befebaae48d5acf37543b14f28b09470b373a935
Callum Rogers <callumr@palantir.com>
2013-10-18 00:30:48 +0100

    Played around with haskell-source-ext

--- src/Main.hs
+++ src/Main.hs
@@ -3,15 +3,41 @@ module Main where
 import Control.Monad
 import Data.Functor.Identity
 import Text.Parsec
-import Language.TypeScript
+import qualified Language.TypeScript as T
+import qualified Language.Haskell.Exts.Syntax as H
+import Language.Haskell.Exts.Pretty (prettyPrint)
 
 main = putStrLn "hi"
 
-parseTypeScript :: String -> Either ParseError [DeclarationElement]
-parseTypeScript = runIdentity . runParserT declarationSourceFile () ""
+parseTypeScript :: String -> Either ParseError [T.DeclarationElement]
+parseTypeScript = runIdentity . runParserT T.declarationSourceFile () ""
 
-parseFile :: String -> IO (Either ParseError [DeclarationElement])
+parseFile :: String -> IO (Either ParseError [T.DeclarationElement])
 parseFile = liftM parseTypeScript . readFile
 
 compile :: String -> String
 compile = id
+
+mapPredefType :: T.PredefinedType -> H.Type
+mapPredefType predef = H.TyCon . H.UnQual . H.Ident $
+	case predef of
+		T.AnyType -> error "any type"
+		T.NumberType -> "Float"
+		T.BooleanType -> "Boolean"
+		T.StringType -> "String"
+		T.VoidType -> "()"
+
+sl :: H.SrcLoc
+sl = H.SrcLoc "" 0 0
+
+makeFFICall :: String -> H.Exp
+makeFFICall name = H.App (H.Var $ H.UnQual (H.Ident "ffi")) (H.Lit $ H.String name)
+
+makeFFI :: String -> H.Type -> [H.Decl]
+makeFFI name ty = [typeSig, func]
+	where
+		typeSig = H.TypeSig sl [H.Ident name] ty
+		func = H.PatBind sl (H.PVar $ H.Ident name) Nothing (H.UnGuardedRhs $ makeFFICall name) (H.BDecls [])
+
+prettyPrintFFI :: String -> String
+prettyPrintFFI = prettyPrint . makeFFICall
